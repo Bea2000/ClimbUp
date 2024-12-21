32a5fefda0e311a28172a6f8d3e0ee2ef3f0f1f1
Gustavo Selman <gustavo.selman@uc.cl>
2024-12-21 11:32:27 -0300

    refac: add components directory in competitions
    delete unused component

--- src/app/competitions/new/page.tsx
+++ src/app/competitions/new/page.tsx
@@ -1,6 +1,6 @@
-import CompetitionForm from '../../components/CompetitionForm';
+import CompetitionForm from '../components/CompetitionForm';
 
-export default function NewCompetition() {
+export default function NewCompetitionPage() {
   return (
     <main className="min-h-screen bg-gray-50 py-12">
       <CompetitionForm />
