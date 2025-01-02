072c1e62ce9e7e6f13e9619567b505198c2326f0
Bea2000 <beatriz.errazuriz@uc.cl>
2025-01-02 16:08:31 -0300

    feat: :sparkles: add scan participant for judge form

---
+++ src/app/welcome/competitions/show/[encodedCompetitionId]/[endodedJudgeId]/page.tsx
@@ -0,0 +1,21 @@
+import { decodeIdInBloat } from "@/lib/encoder";
+
+import ParticipantSearchForm from "./components/ParticipantSearchForm";
+
+type ShowCompetitionPageProps = {
+  params: {
+    encodedCompetitionId: string;
+    endodedJudgeId: string;
+  };
+};
+
+export default async function ShowCompetitionPage({ params }: ShowCompetitionPageProps) {
+  const { encodedCompetitionId, endodedJudgeId } = await params;
+  const competitionId = decodeIdInBloat(encodedCompetitionId);
+  const judgeId = decodeIdInBloat(endodedJudgeId);
+  return (
+    <div>
+      <ParticipantSearchForm competitionId={competitionId} judgeId={judgeId} />
+    </div>
+  );
+}
