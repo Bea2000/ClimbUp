ba0b9e7cf9524f37ec0e8a4991f6f2333e6bcf5d
Bea2000 <beatriz.errazuriz@uc.cl>
2025-04-27 23:26:33 -0400

    feat(participant): ParticipantProblems functions to manage problems

--- src/app/actions/participant.ts
+++ src/app/actions/participant.ts
@@ -42,3 +42,55 @@ export async function searchParticipant(_prevState: unknown, formData: FormData,
     };
   }
 } 
+
+export async function addProblemToParticipant(participantId: number, problemId: number, competitionId: number) {
+  const participant = await findParticipantWithCompetitionAndProblemsByIdAndCompetitionId(participantId, competitionId);
+  if (!participant) {
+    return {
+      status: 'error',
+      error: { message: ['No se encontró ningún participante con ese código'] },
+    };
+  }
+
+  let problem = null;
+
+  if (participant.competition.problems.length > 0) {
+    problem = participant.competition.problems.find((problem) => problem.problemId === problemId);
+  }
+
+  if (!problem) {
+    await assignProblemToParticipant(participantId, problemId, competitionId);
+  }
+
+  return {
+    status: 'success',
+    data: { participant },
+  };
+}
+
+export async function addProblemInformationToParticipant(participantId: number, problemId: number, competitionId: number, completed: boolean) {
+  const participant = await findParticipantWithCompetitionAndProblemsByIdAndCompetitionId(participantId, competitionId);
+  if (!participant) {
+    return {
+      status: 'error',
+      error: { message: ['No se encontró ningún participante con ese código'] },
+    };
+  }
+
+  let problem = null;
+  problem = participant.competition.problems.find((problem) => problem.problemId === problemId);
+  if (!problem) {
+    problem = await assignProblemToParticipant(participantId, problemId, competitionId);
+  }
+  
+  if (completed) {
+    await updateParticipantProblemInformation(problemId, participantId, competitionId, { completed, attempts: problem.attempts + 1 });
+  } else {
+    await updateParticipantProblemInformation(problemId, participantId, competitionId, { attempts: problem.attempts + 1 });
+  }
+
+  return {
+    status: 'success',
+    data: { participant },
+  };
+}
--- src/lib/db/problem.ts
+++ src/lib/db/problem.ts
@@ -1,5 +1,8 @@
+import { ParticipantProblem } from "@prisma/client";
+
 import { CreateProblem } from "@/types/problem";
 
+import { getParticipantCompetitionByParticipantIdAndCompetitionId } from "./participant";
 import prisma from "./prisma";
 
 export async function addProblemToCompetition(competitionId: number, problem: CreateProblem) {
@@ -44,5 +47,52 @@ export async function getProblemsWithJudgesByCompetitionId(competitionId: number
     include: {
       judges: true,
     },
+  });
+}
+
+export async function assignProblemToParticipant(participantId: number, problemId: number, competitionId: number) {
+  const participantCompetition = await getParticipantCompetitionByParticipantIdAndCompetitionId(participantId, competitionId);
+
+  if (!participantCompetition) {
+    throw new Error('Participant competition not found');
+  }
+  
+  return await prisma.participantProblem.create({
+    data: {
+      participantCompetitionId: participantCompetition.id,
+      participantId,
+      problemId,
+    },
+  });
+}
+
+export async function getParticipantProblemByProblemIdAndParticipantIdAndCompetitionId(problemId: number, participantId: number, competitionId: number) {
+  return await prisma.participantProblem.findFirst({
+    where: {
+      problemId,
+      participantId,
+      participantCompetitionId: competitionId,
+    },
+  });
+}
+
+export async function updateParticipantProblemInformation(problemId: number, participantId: number, competitionId: number, data: Partial<ParticipantProblem>) {
+  const participantCompetition = await getParticipantCompetitionByParticipantIdAndCompetitionId(participantId, competitionId);
+
+  if (!participantCompetition) {
+    throw new Error('Participant competition not found');
+  }
+
+  const participantProblem = await getParticipantProblemByProblemIdAndParticipantIdAndCompetitionId(problemId, participantId, participantCompetition.id);
+
+  if (!participantProblem) {
+    throw new Error('Participant problem not found');
+  }
+
+  return await prisma.participantProblem.update({
+    where: {
+      id: participantProblem.id,
+    },
+    data,
   });
 }
