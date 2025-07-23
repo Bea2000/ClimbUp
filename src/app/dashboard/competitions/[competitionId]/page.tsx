46c3bc049384c67e786eaa481d171a67c1e5c779
Gustavo Selman <gustavo.selman@uc.cl>
2025-07-23 16:50:31 -0400

    feat: add manage problems link to ProblemsList component

--- src/app/dashboard/competitions/[competitionId]/page.tsx
+++ src/app/dashboard/competitions/[competitionId]/page.tsx
@@ -77,7 +77,7 @@ export default async function CompetitionPage(props: CompetitionPageProps) {
 
           <div className="divider"></div>
           
-          <ProblemsList problems={competition.problems} />
+          <ProblemsList problems={competition.problems} competitionId={competition.id} />
 
           <div className="divider"></div>
           
